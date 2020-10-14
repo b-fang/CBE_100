e38bce0d1b78156e36fcce20a22df37101bf82f2
brian fang <brianfang@ucla.edu>
2020-10-13 21:31:23 -0700

    initial commit

---
+++ CBE100_HW1.py
@@ -0,0 +1,13 @@
+
+import matplotlib
+from matplotlib import pyplot as plt
+
+def problem3():
+    plt.scatter(1, 1)
+    plt.savefig("CBE100_HW1_Outputs/Problem 3")
+
+def problem4():
+    pass
+
+def problem5():
+    pass
---
+++ main.py
@@ -0,0 +1,7 @@
+
+import CBE100_HW1
+
+if __name__ == '__main__':
+    CBE100_HW1.Prob3()
+
+
